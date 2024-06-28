c4cf3d2171d60cf19af5f6ba6c01958a3c09519a
kyj4528 <83503436+kyj4528@users.noreply.github.com>
2024-06-28 13:12:17 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-        long answer = 0;
+        int answer = 0;
         for(int i=0; i<a.length; i++){
             answer += a[i] * b[i];
         }
-        return (int)answer;
+        return answer;
     }
 }
