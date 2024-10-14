c456e43c9d5dc503f3f00fe6067c994a2e162787
kyj4528 <83503436+kyj4528@users.noreply.github.com>
2024-10-14 20:03:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(int x) {
+        String s = String.valueOf(x);
+        int sum = 0;
+        for(int i=0; i<s.length(); i++){
+            sum += s.charAt(i)-'0';
+        }
+        return x % sum == 0 ? true : false;
+    }
+}
